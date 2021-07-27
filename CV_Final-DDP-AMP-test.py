9d2bd52ff45e95bffced95390f95ee52c8244726
clubzip <ns4809@unist.ac.kr>
2021-07-27 11:42:10 +0000

    fix: use seperate BN module for each BN layer

--- CV_Final-DDP-AMP-test.py
+++ CV_Final-DDP-AMP-test.py
@@ -37,15 +37,20 @@ class CPM2DPose(nn.Module):
         self.relu = F.leaky_relu
         self.conv1_1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=True)  # conv0_1
         self.conv1_2 = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1, bias=True)  # conv0_2
-        self.batnorm1 = nn.BatchNorm2d(64)
+        self.batnorm1_1 = nn.BatchNorm2d(64)
+        self.batnorm1_2 = nn.BatchNorm2d(64)
         self.conv2_1 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1, bias=True)  # conv0_2
         self.conv2_2 = nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1, bias=True)  # conv0_2
-        self.batnorm2 = nn.BatchNorm2d(128)
+        self.batnorm2_1 = nn.BatchNorm2d(128)
+        self.batnorm2_2 = nn.BatchNorm2d(128)
         self.conv3_1 = nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1, bias=True)  # conv0_2
         self.conv3_2 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1, bias=True)  # conv0_2
         self.conv3_3 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1, bias=True)  # conv0_2
         self.conv3_4 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1, bias=True)  # conv0_2
-        self.batnorm3 = nn.BatchNorm2d(256)
+        self.batnorm3_1 = nn.BatchNorm2d(256)
+        self.batnorm3_2 = nn.BatchNorm2d(256)
+        self.batnorm3_3 = nn.BatchNorm2d(256)
+        self.batnorm3_4 = nn.BatchNorm2d(256)
         self.conv4_1 = nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1, bias=True)  # conv0_2
         self.conv4_2 = nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1, bias=True)  # conv0_2
         self.conv4_3 = nn.Conv2d(512, 256, kernel_size=3, stride=1, padding=1, bias=True)  # conv0_2
@@ -53,7 +58,9 @@ class CPM2DPose(nn.Module):
         self.conv4_5 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1, bias=True)  # conv0_2
         self.conv4_6 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1, bias=True)  # conv0_2
         self.conv4_7 = nn.Conv2d(256, 128, kernel_size=3, stride=1, padding=1, bias=True)  # conv0_2
-        self.batnorm4 = nn.BatchNorm2d(256)
+        self.batnorm4_1 = nn.BatchNorm2d(256)
+        self.batnorm4_2 = nn.BatchNorm2d(256)
+        self.batnorm4_3 = nn.BatchNorm2d(256)
         self.conv5_1 = nn.Conv2d(128, 512, kernel_size=1, stride=1, padding=0, bias=True)  # conv0_2
         self.conv5_2 = nn.Conv2d(512, 21, kernel_size=1, stride=1, padding=0, bias=True)  # conv0_2
         self.conv6_1 = nn.Conv2d(149, 128, kernel_size=7, stride=1, padding=3, bias=True)  # conv0_2
@@ -63,7 +70,11 @@ class CPM2DPose(nn.Module):
         self.conv6_5 = nn.Conv2d(128, 128, kernel_size=7, stride=1, padding=3, bias=True)  # conv0_2
         self.conv6_6 = nn.Conv2d(128, 128, kernel_size=1, stride=1, padding=0, bias=True)  # conv0_2
         self.conv6_7 = nn.Conv2d(128, 21, kernel_size=1, stride=1, padding=0, bias=True)  # conv0_2
-        self.batnorm6 = nn.BatchNorm2d(128)
+        self.batnorm6_1 = nn.BatchNorm2d(128)
+        self.batnorm6_2 = nn.BatchNorm2d(128)
+        self.batnorm6_3 = nn.BatchNorm2d(128)
+        self.batnorm6_4 = nn.BatchNorm2d(128)
+        self.batnorm6_5 = nn.BatchNorm2d(128)
         self.conv7_1 = nn.Conv2d(149, 128, kernel_size=7, stride=1, padding=3, bias=True)  # conv0_2
         self.conv7_2 = nn.Conv2d(128, 128, kernel_size=7, stride=1, padding=3, bias=True)  # conv0_2
         self.conv7_3 = nn.Conv2d(128, 128, kernel_size=7, stride=1, padding=3, bias=True)  # conv0_2
@@ -71,7 +82,10 @@ class CPM2DPose(nn.Module):
         self.conv7_5 = nn.Conv2d(128, 128, kernel_size=7, stride=1, padding=3, bias=True)  # conv0_2
         self.conv7_6 = nn.Conv2d(128, 128, kernel_size=1, stride=1, padding=0, bias=True)  # conv0_2
         self.conv7_7 = nn.Conv2d(128, 21, kernel_size=1, stride=1, padding=0, bias=True)  # conv0_2
-        self.batnorm7 = nn.BatchNorm2d(128)
+        self.batnorm7_1 = nn.BatchNorm2d(128)
+        self.batnorm7_2 = nn.BatchNorm2d(128)
+        self.batnorm7_3 = nn.BatchNorm2d(128)
+        self.batnorm7_4 = nn.BatchNorm2d(128)
         self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
         self.dropout = nn.Dropout2d(p=0.1)
 
@@ -95,32 +109,32 @@ class CPM2DPose(nn.Module):
     @torch.cuda.amp.autocast()
     def forward(self, x):
         x = self.relu(self.conv1_1(x))
-        x = self.batnorm1(x)
+        x = self.batnorm1_1(x)
         x = self.relu(self.conv1_2(x))
         x = self.maxpool(x)
-        x = self.batnorm1(x)
+        x = self.batnorm1_2(x)
         x = self.relu(self.conv2_1(x))
-        x = self.batnorm2(x)
+        x = self.batnorm2_1(x)
         x = self.relu(self.conv2_2(x))
         x = self.maxpool(x)
-        x = self.batnorm2(x)
+        x = self.batnorm2_2(x)
         x = self.relu(self.conv3_1(x))
-        x = self.batnorm3(x)
+        x = self.batnorm3_1(x)
         x = self.relu(self.conv3_2(x))
-        x = self.batnorm3(x)
+        x = self.batnorm3_2(x)
         x = self.relu(self.conv3_3(x))
-        x = self.batnorm3(x)
+        x = self.batnorm3_3(x)
         x = self.relu(self.conv3_4(x))
         x = self.maxpool(x)
-        x = self.batnorm3(x)
+        x = self.batnorm3_4(x)
         x = self.relu(self.conv4_1(x))
         x = self.relu(self.conv4_2(x))
         x = self.relu(self.conv4_3(x))
-        x = self.batnorm4(x)
+        x = self.batnorm4_1(x)
         x = self.relu(self.conv4_4(x))
-        x = self.batnorm4(x)
+        x = self.batnorm4_2(x)
         x = self.relu(self.conv4_5(x))
-        x = self.batnorm4(x)
+        x = self.batnorm4_3(x)
         x = self.relu(self.conv4_6(x))
         encoding = self.relu(self.conv4_7(x))
         x = self.relu(self.conv5_1(encoding))
@@ -128,28 +142,28 @@ class CPM2DPose(nn.Module):
 
         x = torch.cat([scoremap, encoding], 1)
         x = self.relu(self.conv6_1(x))
-        x = self.batnorm6(x)
+        x = self.batnorm6_1(x)
         x = self.relu(self.conv6_2(x))
-        x = self.batnorm6(x)
+        x = self.batnorm6_2(x)
         x = self.relu(self.conv6_3(x))
-        x = self.batnorm6(x)
+        x = self.batnorm6_3(x)
         x = self.relu(self.conv6_4(x))
-        x = self.batnorm6(x)
+        x = self.batnorm6_4(x)
         x = self.relu(self.conv6_5(x))
-        x = self.batnorm6(x)
+        x = self.batnorm6_5(x)
         x = self.relu(self.conv6_6(x))
         scoremap = self.conv6_7(x)
         x = torch.cat([scoremap, encoding], 1)
         x = self.relu(self.conv7_1(x))
-        x = self.batnorm7(x)
+        x = self.batnorm7_1(x)
         x = self.relu(self.conv7_2(x))
-        x = self.batnorm7(x)
+        x = self.batnorm7_2(x)
         x = self.relu(self.conv7_3(x))
-        x = self.batnorm7(x)
+        x = self.batnorm7_3(x)
         x = self.relu(self.conv7_4(x))
         x = self.dropout(x)
         x = self.relu(self.conv7_5(x))
-        x = self.batnorm7(x)
+        x = self.batnorm7_4(x)
         x = self.relu(self.conv7_6(x))
         x = self.conv7_7(x)
         return x
